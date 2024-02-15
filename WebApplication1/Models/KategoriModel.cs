449283f9a271987c38a263e7a082825d348cfcbb
Mehmet4602can <mehmetcelik4602@gmail.com>
2024-02-15 09:15:55 +0300

    Proje dosyası ekle.

---
+++ WebApplication1/Models/KategoriModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class KategoriModel
+    {
+        public int KategoriId { get; set; }
+        public string KategoriName { get; set; }
+        List<KitapModel> BookList = new List<KitapModel>();
+    }
+}
---
+++ WebApplication1/Models/KitapModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class KitapModel
+    {
+        public int bookId { get; set; }
+        public string bookName { get; set; }
+        public int sayfaSayısı { get; set; }
+    }
+}
